05b0c01c1d5afa04e373578a038266a846afb7cb
Michelle Adams <yourgithubemail@example.com>
2026-05-04 00:18:50 +0000

    add

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import NavBar from "../components/layout/NavBar";
+import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 
 export default function Learn() {
@@ -48,7 +48,7 @@ export default function Learn() {
 
   return (
     <div className="min-h-screen bg-white flex flex-col">
-      <NavBar />
+      <Navbar />
 
       <main className="flex-1">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-16 lg:py-20">
